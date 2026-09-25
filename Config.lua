6ea90004cef2d4677cc62ebd6ffe826d30a84f0b
pangaustralia <alvialdeangelo@Gmail.com>
2026-09-25 19:38:53 +0800

    Add files via upload

---
+++ Config.lua
@@ -0,0 +1,31 @@
+-- ==================================================
+-- YOKUDO HUB | NEW PROJECT | Config
+-- ==================================================
+
+_G.YOKUDO = {
+    -- Hub Info
+    Name = "ola | Steal An Egg",
+    Version = "ig : @nganowhybakit",
+    Author = "ola",
+
+    -- Asset
+    AssetID = "rbxassetid://81526905178572",
+
+    -- UI
+    UI = {
+        Width = 480,
+        Height = 340,
+        SidebarWidth = 115,
+        TabHeight = 32,
+        Theme = {
+            Background = Color3.fromRGB(16, 17, 23),
+            Sidebar = Color3.fromRGB(20, 21, 28),
+            TopBar = Color3.fromRGB(23, 24, 32),
+            Accent = Color3.fromRGB(105, 90, 190),
+            Text = Color3.fromRGB(255, 255, 255),
+            SubText = Color3.fromRGB(145, 145, 165),
+        }
+    },
+}
+
+print("✅ Config Loaded")
