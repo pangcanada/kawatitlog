4c304b6d0cff3ff3a242bc06b2c5551c1d8d0a4d
pangaustralia <alvialdeangelo@Gmail.com>
2026-09-25 19:39:31 +0800

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -4,7 +4,7 @@
 -- ✅ VIPTP ជំនួស EggCheckPremium
 -- ==================================================
 
-local BASE_URL = "https://raw.githubusercontent.com/nekol-ann/steala/ola/"
+local BASE_URL = "https://raw.githubusercontent.com/pangcanada/kawatitlog/main/"
 
 _G.YOKUDO_EnablePrint = false
 
